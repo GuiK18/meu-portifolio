fce80170f4deabc6d5122f5f50365f4c18070559
GuiK18 <guilhermek1805@gmail.com>
2026-02-01 15:54:46 -0300

    acréscimo do arquivo JS
    Este comando no arquivo JS elimina a necessidade de repetição no HTML.

---
+++ js/index.js
@@ -0,0 +1,20 @@
+lista_links =
+[
+    {logo: "imagens/logo_github.png", nome: "GITHUB", link: "https://github.com/GuiK18"},
+    {logo: "imagens/logo_linkedin.png", nome: "LINKEDIN", link: "https://www.linkedin.com/in/guilherme-kavalco-15831434a/"}
+];
+
+
+for(var i = 0; i < lista_links.length; i++){
+    var html = `<div class="links">
+                    <div class="logo">
+                        <img src="${lista_links[i].logo}" width="60" height="60">
+                    </div>
+                    <div class="nome_acesso">${lista_links[i].nome}</div>
+                    <div class="logo">
+                        <img src="${lista_links[i].logo}" width="60" height="60">
+                    </div>
+                </div>`;
+
+    document.getElementById('informacoes').innerHTML += html;
+}
